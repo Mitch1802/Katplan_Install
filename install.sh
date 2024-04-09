13d90d0f65c0fa81461ba162edcf1c19f971382a
Mitch1802 <m.r.96@gmx.net>
2024-04-09 23:38:36 +0200

    initial

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -e
+echo "Katplan Installation startet!"
+
+PROJECT_GIT_URL='https://github.com/Mitch1802/Katplan_Install.git'
+PROJECT_BASE_PATH='/srv/katplan/'
+DOCKER_NETWORK_NAME='katplan_nw'
+
+if [ ! -d $PROJECT_BASE_PATH ]
+then
+  mkdir -p $PROJECT_BASE_PATH
+fi
+
+sudo git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH
+
+sudo docker login --username mitch122 --password-stdin
+sudo docker pull mitch122/katplan:nginx-2.0.0
+sudo docker pull mitch122/katplan:db-2.0.0
+sudo docker pull mitch122/katplan:api-2.0.0
+
+cd $PROJECT_BASE_PATH
+sudo docker network create -d bridge $DOCKER_NETWORK_NAME
+sudo docker compose up --build -d --remove-orphans
+sleep 30
+sudo docker compose exec api python manage.py loaddata /app/backups/initial.json
+
+echo "Katplan Installation erfolgreich durchgeführt!"
