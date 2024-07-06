b1c14d23187b585664b3070d7ada8d8f72ec046c
Mitch1802 <m.r.96@gmx.net>
2024-07-07 01:59:48 +0200

    test

--- update.sh
+++ update.sh
@@ -16,22 +16,25 @@ fi
 sudo git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH
 
 echo "Stoppe Docker Container"
-sudo docker stop katplan_nginx
-sudo docker stop katplan_db
-sudo docker stop katplan_api
-sudo docker remove katplan_nginx
-sudo docker remove katplan_db
-sudo docker remove katplan_api
+sudo docker stop katplan_2432_nginx
+sudo docker stop katplan_2432_db
+sudo docker stop katplan_2432_api
 
-echo "Lade Docker Images..."
+echo "Enterne Docker Container"
+sudo docker remove katplan_2432_nginx
+sudo docker remove katplan_2432_db
+sudo docker remove katplan_2432_api
+
+echo "Lade Docker Images"
 sudo docker pull mitch122/katplan:nginx-$TAG
 sudo docker pull mitch122/katplan:db-$TAG
 sudo docker pull mitch122/katplan:api-$TAG
-echo "Starte Docker..."
+
+echo "Starte Docker"
 cd $PROJECT_BASE_PATH
 sudo docker compose up --build -d --remove-orphans
 
-echo "Starte Docker..."
+echo "Entferne Install/Update Datein"
 sudo rm install.sh update.sh README.md
 sudo rm -r .git
 echo "Katplan Update erfolgreich durchgeführt!"
