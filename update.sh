b571617ff27ce8c23a66f05a7143a1feae327bbd
Mitch1802 <m.r.96@gmx.net>
2024-07-11 23:59:09 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,17 +3,15 @@
 set -e
 echo "Katplan Installation startet!"
 
-PROJECT_GIT_URL='https://github.com/Mitch1802/Katplan_Install.git'
 PROJECT_BASE_PATH='/srv/katplan_2432/'
 TAG='2.1.0'
 
 if [ ! -d $PROJECT_BASE_PATH ]
-  # sudo rm -r /srv/katplan_2432
 then
   mkdir -p $PROJECT_BASE_PATH
 fi
 
-# sudo git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH
+
 cd $PROJECT_BASE_PATH
 
 echo "Herunterladen neuer Konfigurationen..."
@@ -38,7 +36,4 @@ sudo docker pull mitch122/katplan:api-$TAG
 echo "Starte Anwendung..."
 sudo docker compose up --build -d --remove-orphans
 
-# echo "Entferne Install/Update Datein"
-# sudo rm install.sh update.sh README.md
-# sudo rm -r .git
 echo "Katplan Update erfolgreich durchgeführt!"
